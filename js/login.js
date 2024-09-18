3530b0c974d96017ab82413662e0654dd0935d70
Rubaiat15 <rubaiatferdous15@gmail.com>
2024-09-18 22:54:33 +0600

    ki hoilo

--- js/login.js
+++ js/login.js
@@ -1,6 +1,18 @@
 document.getElementById('btn-login').addEventListener('click',function(event){
     event.preventDefault();
-    console.log('clicked');
+    
      const phoneNumber = document.getElementById('phone-number').value ;
      console.log(phoneNumber);
+     const pinNo = document.getElementById('pin-no').value ;
+     console.log(pinNo);
+
+     if(phoneNumber==='5' && pinNo==='1234'){
+        console.log("Logged in")
+        window.location.href = '/home.html';
+
+     }
+
+     else{
+        alert("wrong number or pin");
+     }
 })
