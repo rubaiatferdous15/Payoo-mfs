57ae295d7d78df7eb7d038b5c338205b6d287c26
Rubaiat15 <rubaiatferdous15@gmail.com>
2024-09-18 23:42:24 +0600

    era kara

--- js/home.js
+++ js/home.js
@@ -1,1 +1,11 @@
-console.log('hoise')
+document.getElementById('btn-money').addEventListener('click',function(event){
+    event.preventDefault();
+    const addMoneyInput = document.getElementById('add-money').value ;
+    console.log(addMoneyInput)
+    const pinNo= document.getElementById('input-pin'). value;
+    console.log(pinNo);
+
+    if(pinNo==="1224"){
+        
+    }
+})
